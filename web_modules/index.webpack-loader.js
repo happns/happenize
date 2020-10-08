ea3a53e7e03dfd3a6ea394d816884687833d5452
Marek Micek <mmicek@happns.io>
2020-10-08 13:36:42 +0200

    Update index.webpack-loader.js

--- web_modules/index.webpack-loader.js
+++ web_modules/index.webpack-loader.js
@@ -6,7 +6,7 @@ module.exports = function(content) {
 	this.addContextDependency(this.context);
 
 	if (content.indexOf("'auto-generate'") !== -1) {
-		return generateIndexForDirectory({ path: this.content });
+		return generateIndexForDirectory({ path: this.context });
 	}
 	
 	return content;
